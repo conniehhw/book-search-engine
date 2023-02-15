8040b7fd67214bbba6d7db71869d291b0c0c6895
Connie Wong <conniehhw@gmail.com>
2023-02-15 17:22:53 -0500

    Update file to work for GraphQL, schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,30 @@
+const { User, Book } = require("../models");
+
+const resolvers = {
+  Query: {
+    user: async () => {
+      return User.find({});
+    },
+    books: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Book.find(params);
+    },
+  },
+};
+//   Mutation: {                                         // reread on Mutation
+//     createMatchup: async (parent, args) => {
+//       const matchup = await Matchup.create(args);
+//       return matchup;
+//     },
+//     createVote: async (parent, { _id, techNum }) => {
+//       const vote = await Matchup.findOneAndUpdate(
+//         { _id },
+//         { $inc: { [`tech${techNum}_votes`]: 1 } },
+//         { new: true }
+//       );
+//       return vote;
+//     },
+//   },
+// };
+
+module.exports = resolvers;
